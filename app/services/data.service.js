143022cbeb8a6afd9f60e7f53757209cfd70644c
unknown <sanjeetkumarit@outlook.com>
2025-01-31 15:03:45 +0530

    more

--- app/services/data.service.js
+++ app/services/data.service.js
@@ -1,7 +1,8 @@
 app.service('DataService', function($http) {
     this.getData = function() {
-        return $http.get('/data.json').then(function(response) {
-            return response.data; // Extract actual data
-        });
+        return $http.get('/data.json')  // Assuming data.json is in the root folder
+            .then(function(response) {
+                return response.data;  // Return the actual data from the response
+            });
     };
 });
